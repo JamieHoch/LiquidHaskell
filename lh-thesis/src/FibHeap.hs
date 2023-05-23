c3af9acbbfcdf3edde71de628ce7adefab103dd7
jamie <jamie.hochrainer@student.uibk.ac.at>
2023-05-23 09:42:53 +0200

    better refinement of link, merge, singleton, insert: new root is min

--- lh-thesis/src/FibHeap.hs
+++ lh-thesis/src/FibHeap.hs
@@ -5,15 +5,10 @@
 
 module FibHeap
     (
-        singleton
-        , makeHeap
-        , insert
-        , findMin
-        , extractMin
-        , merge
-        , deleteMin
+        
     ) where
 -- mergeable heap: makeHeap, insert, findMin, extractMin, union=merge
+import Prelude
 
 {-@ type Pos = {v:Int | 0 < v} @-}
 {-@ type NEFibHeap = {v : FibHeap a | notEmptyFibHeap v} @-}
@@ -46,12 +41,13 @@ data FibHeap a = E | FH { minTree :: FibTree a
                         , trees :: [FibTree a] --wihout minTree
                      }
 
+
 {-@ isMin ::  FibTree a -> [FibTree a] -> Bool @-}
 isMin :: (Ord a) => FibTree a -> [FibTree a] -> Bool
 isMin t [] = True
 isMin t (t':ts) = root t <= root t' && isMin t ts
 
-{-@ measure ranksum @-}  
+{-@ measure ranksum @-}
 ranksum :: [FibTree a] -> Int
 ranksum [] = 0
 ranksum (t:ts) = rank t + ranksum ts
@@ -61,43 +57,50 @@ notEmptyFibHeap :: FibHeap a -> Bool
 notEmptyFibHeap E = False
 notEmptyFibHeap _ = True
 
+{-@ measure isEmptyFibHeap @-}
+{-@ isEmptyFibHeap :: t:(FibHeap a) -> {t':Bool | (not (notEmptyFibHeap t) && t') || (notEmptyFibHeap t && not t')} @-}
+isEmptyFibHeap :: FibHeap a -> Bool 
+isEmptyFibHeap E = True
+isEmptyFibHeap _ = False
+
 {-@ makeHeap :: EFibHeap @-}
 makeHeap :: FibHeap a
 makeHeap = E
 
-{-@ singleton :: a -> {v: NEFibHeap | trees v = [] && subtrees (minTree v) = [] && rank (minTree v) = 1}@-}
+{-@ singleton :: x:a -> {v: NEFibHeap | Rmin v == x && trees v = [] && subtrees (minTree v) = [] && rank (minTree v) = 1}@-}
 singleton :: a -> FibHeap a
 singleton x = FH (Node x 1 [] False) []
 
-{-@ link :: t1:FibTree a -> {t2:FibTree a | rank t1 == rank t2} -> {v:FibTree a | rank v == 1 + (rank t1)} @-}
+{-@ link :: t1:FibTree a -> {t2:FibTree a | rank t1 == rank t2} -> {v:FibTree a | rank v == 1 + (rank t1) && (root v == root t1 && root t1 <= root t2 || root v == root t2 && root t2 <= root t1)} @-}
 link :: (Ord a) => FibTree a -> FibTree a -> FibTree a
 link t1@(Node x r c1 _) t2@(Node y _ c2 _)
     | x < y = Node x (r+1) (t2:c1) False
     | otherwise = Node y (r+1) (t1:c2) False
 
+{-@ predicate Rmin T = root (minTree T) @-}
 -- constant time
-{-@ merge :: FibHeap a -> NEFibHeap -> NEFibHeap @-}
+{-@ merge :: t:(FibHeap a) -> t':NEFibHeap -> {v:NEFibHeap | Rmin v == Rmin t && Rmin t < Rmin t' || Rmin v == Rmin t' && (Rmin t' <= Rmin t || isEmptyFibHeap t)} @-}
 merge:: (Ord a) => FibHeap a -> FibHeap a -> FibHeap a
 merge E h = h
 merge h1@(FH minTr1 ts1) h2@(FH minTr2 ts2)
     | root minTr1 < root minTr2 = FH minTr1 (minTr2:ts2++ts1)
     | otherwise = FH minTr2 (minTr1:ts1++ts2)
 
--- constant time
-{-@ insert :: FibHeap a -> a -> NEFibHeap @-}
+-- constant time  | Rmin v == Rmin t && Rmin t <= x || Rmin v == x && (x <= Rmin t || isEmptyFibHeap t)} 
+{-@ insert :: t:(FibHeap a) -> x:a -> {v:NEFibHeap | Rmin v == Rmin t && Rmin t <= x || Rmin v == x && (x <= Rmin t || isEmptyFibHeap t)}  @-}
 insert :: (Ord a) => FibHeap a -> a -> FibHeap a
 insert h x = merge h (singleton x)
 
 findMin :: (Ord a) => FibHeap a -> a
 findMin = root . minTree
 
-{-@ meld :: [FibTree a] -> FibTree a -> {t:[FibTree a] | len t > 0}@-}
+{-@ meld :: t:[FibTree a] -> FibTree a -> {v:[FibTree a] | len v > 0 && len v <= len t +1}@-}
 meld :: Ord a => [FibTree a] -> FibTree a -> [FibTree a]
 meld [] t = [t]
 meld (t':ts) t = if rank t' == rank t then meld ts (link t t')
                      else t:t':ts
 
-{-@ meld' :: {t:[FibTree a] | len t > 0} -> [FibTree a] -> {t:[FibTree a] | len t > 0} @-}
+{-@ meld' :: {t:[FibTree a] | len t > 0} -> t':[FibTree a] -> {v:[FibTree a] | len v > 0 && len v <= len t + len t'} @-}
 meld' :: Ord a => [FibTree a] -> [FibTree a] -> [FibTree a]
 meld' (t:ts) [] = (t:ts)
 meld' (t':ts') (t:ts) = if rank t' == rank t 
@@ -119,7 +122,7 @@ extractMin (t:ts) =
 
 {-@ deleteMin :: h:NEFibHeap -> v: FibHeap a @-}
 deleteMin :: (Ord a) => FibHeap a -> FibHeap a
-deleteMin (FH (Node x _ [] _) []) = E
+deleteMin (FH (Node x _ [] _) [] ) = E
 deleteMin h@(FH minTr ts@(x:xs)) = FH minTr' ts' where
     (minTr', ts') = extractMin $ consolidate (subtrees minTr ++ ts)
 deleteMin h@(FH minTr@(Node _ _ subtr@(x:xs) _) ts) = FH minTr' ts' where
