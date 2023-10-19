6076979516bc0f436841f0f7144c73abefac0c19
Niki Vazou <nikivazou@gmail.com>
2023-10-19 12:42:52 +0200

    abind

--- lh-thesis/src/PotentialAnalysis_Binomial2.hs
+++ lh-thesis/src/PotentialAnalysis_Binomial2.hs
@@ -91,6 +91,8 @@ pot :: [a] -> Int
 pot []     = 0
 pot (x:xs) = 1 + (pot xs)
 
+{-@ invariant {xs:[a] |  pot xs == len xs }@-}
+
 -- potential of tuple
 {-@ measure pott @-}
 {-@ pott :: (BiTree a,[BiTree a]) -> Int @-}
@@ -98,9 +100,22 @@ pott :: (BiTree a,[BiTree a]) -> Int
 pott (x,xs) = pot xs + 1
 
 
+
+-- amortized ti (pot (tval (insTree t ts))) (pot ts) == 2
+-- tcost ti + pot (tval (insTree t ts)) - pot ts = 2
+-- tcost ti = 2 + pot ts -  pot os  >= 2 + pot ts -  pot ts - 1 = 1
+
+-- tcost ti >= 1 
+-- 
+--  pot os <= pot ts + 1
+--  - pot os >= - pot ts - 1 
+-- 
+-- pot (tval (insTree t ts)) <= len ts + 1 
+-- pot (tval (insTree t ts)) <= len ts + 1 
+
 {-@ reflect insTree @-}
 {-@ insTree :: t:BiTree a -> ts:[BiTree a] 
-        -> {ti:(Tick {zs:[BiTree a]| len zs <= len ts + 1}) | tcost ti + pot (tval (insTree t ts)) - pot ts == 2 && pot (tval (insTree t ts)) - pot ts <= 1} @-}
+        -> {ti:(Tick {zs:[BiTree a]| len zs <= len ts + 1}) | amortized ti (pot (tval (insTree t ts))) (pot ts) == 2 && pot (tval (insTree t ts)) - pot ts <= 1 } @-}
 insTree :: Ord a => BiTree a -> [BiTree a] -> Tick [BiTree a]
 insTree t [] = wait [t]
 insTree t ts@(t':ts')
@@ -120,21 +135,37 @@ singleton x = Node 0 x [] 1
 insert :: Ord a => a -> Heap a -> Tick (Heap a)
 insert x ts = insTree (singleton x) ts
 
+
+{-@ inline amortized @-}
+amortized :: Tick a -> Int -> Int -> Int  
+amortized cost out input = tcost cost + out - input
+
 -- tcost ti + pot (tval ti) - (pot ts1 + pot ts2) <= log n
 -- length of list is log n ==> log n = pot (tval ti)
 -- O(log n)
 {-@ reflect mergeHeap @-}
 {-@ mergeHeap :: Ord a => ts1:[BiTree a] -> ts2:[BiTree a] 
-        -> {ti:Tick [BiTree a] | tcost ti + pot (tval (mergeHeap ts1 ts2)) - (pot ts1 + pot ts2) <= pot (tval (mergeHeap ts1 ts2)) && pot (tval (mergeHeap ts1 ts2)) == len (tval (mergeHeap ts1 ts2)) && len (tval (mergeHeap ts1 ts2)) <= len ts1 + len ts2} @-}
+        -> {ti:Tick {xs:[BiTree a] | len xs <= len ts1 + len ts2 && pot xs == len xs} | amortized ti (pot (tval (mergeHeap ts1 ts2))) (pot ts1 + pot ts2) <= pot (tval (mergeHeap ts1 ts2)) } @-}
 mergeHeap :: Ord a => [BiTree a] -> [BiTree a] -> Tick [BiTree a]
 mergeHeap ts1 [] = pure ts1
 mergeHeap [] ts2 = pure ts2
 mergeHeap ts1@(t1:ts1') ts2@(t2:ts2')
     | rank t1 < rank t2 = pure ((:) t1) </> mergeHeap ts1' ts2
     | rank t2 < rank t1 = pure ((:) t2) </> mergeHeap ts1 ts2'
-    | otherwise = waitN 2 (tval (insTree (link t1 t2) (tval (mergeHeap ts1' ts2')))) -- !!CHEAT
-    -- insTree (link t1 t2) RTick.=<< mergeHeap ts1' ts2' -- worst case not amortized
-    -- TODO find out how to use amortized time here
+    | otherwise = abind (pot ts1  + pot ts2) 2 (pot ts1 + pot ts2 + 1) (pot ts1 + pot ts2) (mergeHeap ts1' ts2') (insTree (link t1 t2)) 
+
+
+{-@ reflect abind @-}
+abind :: Int -> Int -> Int -> Int -> Tick a -> (a -> Tick c) -> Tick c
+{-@ abind :: n:Nat -> m:Nat -> a:Nat -> b:Nat -> x:Tick a -> f:(x:a -> {t:Tick c | True}) 
+                 -> {ti:Tick c | (tcost ti  <= n + m ) && tval (f (tval x)) == tval ti  } @-}
+abind _ _ _ _ (Tick c1 x) f = Tick 0 {- 1 + c1 + c2 -} y
+    where Tick c2 y = f x
+
+
+-- We KNOW: tcost ti <= (1 + c1 + c2)
+-- 1 + c1 + c2 <= n
+-- WE WANT (tcost ti  <= n)
 
 {-@ reflect getRoot @-}
 getRoot :: Ord a => BiTree a -> a
@@ -146,7 +177,7 @@ first (x,xs) = x
 
 {-@ reflect removeMinTree @-}
 {-@ removeMinTree :: Ord a => ts:NEHeap a
-        -> {ti:Tick (BiTree a, [BiTree a]) | tcost ti + pott (tval ti) - pot ts <= pot ts && pott (tval ti) == pot ts && tcost ti <= pot ts} @-}
+        -> {ti:Tick {v:(BiTree a, [BiTree a]) | pott v == pot ts} | tcost ti + pott (tval ti) - pot ts <= pot ts && tcost ti <= pot ts} @-}
 removeMinTree :: Ord a => Heap a -> Tick (BiTree a, [BiTree a])
 removeMinTree [t] = pure (t,[])
 removeMinTree (t:ts)
@@ -163,6 +194,7 @@ removeMinTree (t:ts)
 findMin :: Ord a => Heap a -> Tick a
 findMin ts = RTick.liftM getRoot (RTick.liftM first (removeMinTree ts))
 
+{- 
 -- O(log n)
 {-@ reflect deleteMin @-}
 {-@ deleteMin :: Ord a => h:NEHeap a -> Tick (Heap a)@-}
@@ -172,9 +204,10 @@ deleteMin ts = let (Node _ x ts1 _, ts2) = tval (removeMinTree ts) in
 
 {-@ reflect deleteMin' @-}
 {-@ deleteMin' :: Ord a => ts1:[BiTree a] -> ts2:[BiTree a] 
-        -> {h:NEHeap a | pot h == pot ts2 + 1} -> {ti:Tick (Heap a) | tcost ti + pot (tval (deleteMin' ts1 ts2 h)) - pot h <= 2* (pot ts1 + pot ts2) && pot (tval (deleteMin' ts1 ts2 h)) <= pot ts1 + pot ts2} @-}
+        -> {h:NEHeap a | pot h == pot ts2 + 1} -> {ti:Tick ({v: Heap a | pot v <= pot ts1 + pot ts2}) | amortized ti (pot (tval (deleteMin' ts1 ts2 h))) (pot h) <= 2 * (pot ts1 + pot ts2)} @-}
 deleteMin' :: Ord a => [BiTree a] -> [BiTree a] -> Heap a -> Tick (Heap a)
 deleteMin' ts1 ts2 h = RTick.step (tcost (removeMinTree h)) (mergeHeap (myreverse ts1) ts2)
+-}
 
 {-@ reflect myreverse @-}
 {-@ myreverse :: xs:[a] -> {ys:[a] | len xs == len ys } @-}
